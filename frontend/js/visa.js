a8e294a4f0a78ca91a49c7219d9bcca5bda085fd
Martin <ma.carriel@duocuc.cl>
2024-06-25 10:28:55 -0400

    Visa Form Validate

--- frontend/js/visa.js
+++ frontend/js/visa.js
@@ -1,33 +1,60 @@
 $(document).ready(function () {
-    $("#visaForm").submitHandler(alert('Hola!'));
+    $("#visaForm").submit(function(event) {
+        event.preventDefault(); // Evita que el formulario se envíe automáticamente
+
+        return false;
+    });
+
     $("#visaForm").validate({
+        rules: {
+            nombre: {
+                required: true
+            },
+            apellido: {
+                required: true
+            },
+            pais: {
+                required: true
+            },
+            motivodeV: {
+                required: true
+            }
+        },
+        messages: {
+            nombre: {
+                required: "Por favor ingresa tu nombre"
+            },
+            apellido: {
+                required: "Por favor ingresa tu apellido"
+            },
+            pais: {
+                required: "Por favor ingresa tu país de origen"
+            },
+            motivodeV: {
+                required: "Por favor ingresa el motivo de la visa"
+            }
+        },
         submitHandler: function (form) {
             var data = {
-                nombreV: $("#nombreV").val(),
-                apellidoV: $("#apellidoV").val(),
-                paisV: $("#paisV").val(),
-                motivodeV: $("#motivodeV").val()
+                nombre: $("#nombreV").val(),
+                apellido: $("#apellidoV").val(),
+                pais: $("#paisV").val(),
+                motivodeV: $("#motivo").val()
             };
-            console.log(data    );
+            console.log(data);
             $.ajax({
                 url: "http://127.0.0.1:8000/api/tramitevisa/",
                 type: "POST",
                 data: JSON.stringify(data),
                 contentType: "application/json",
                 success: function (response) {
-                    localStorage.setItem("Completado", response.access);
-                    alert("Tramite successful!");
+                    alert("Trámite exitoso!");
                 },
                 error: function (response) {
-                    alert("Tramite failed!");
+                    alert("Error en el trámite");
                 }
             });
+            return false; // Evita que el formulario se envíe automáticamente
         }
     });
 });
-
-
-
-
-
-
