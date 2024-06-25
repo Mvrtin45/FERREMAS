c54ec08b9ce7a25c8c9f984a0102c83671745183
Martin <ma.carriel@duocuc.cl>
2024-06-25 09:52:23 -0400

    VisaForm

--- frontend/js/seguimiento.js
+++ frontend/js/seguimiento.js
@@ -1,20 +1,14 @@
 $(document).ready(function() {
-    // Función para manejar la búsqueda de trámites
+    
     $('#seguimientoForm').submit(function(event) {
-        event.preventDefault(); // Evitar el envío del formulario
+        event.preventDefault(); 
 
-        // Obtener los datos del formulario
         var nombreTramite = $('#nombre_tramite').val();
         var numeroSeguimiento = $('#numero_seguimiento').val();
 
-        // Aquí puedes agregar la lógica para buscar los trámites según los datos proporcionados
-        // Por ejemplo, puedes hacer una solicitud AJAX al servidor para buscar los trámites
-
-        // Por ahora, solo imprimo los datos en la consola para verificar
         console.log('Nombre del trámite:', nombreTramite);
         console.log('Número de seguimiento:', numeroSeguimiento);
 
-        // Mostrar mensaje de búsqueda exitosa (puedes reemplazar esto con tu lógica real)
         alert('Búsqueda de trámite exitosa: ' + nombreTramite);
     });
 });
--- frontend/js/visa.js
+++ frontend/js/visa.js
@@ -1,31 +1,31 @@
 $(document).ready(function () {
+    $("#visaForm").submitHandler(alert('Hola!'));
     $("#visaForm").validate({
         submitHandler: function (form) {
-            subscribe();
+            var data = {
+                nombreV: $("#nombreV").val(),
+                apellidoV: $("#apellidoV").val(),
+                paisV: $("#paisV").val(),
+                motivodeV: $("#motivodeV").val()
+            };
+            console.log(data    );
+            $.ajax({
+                url: "http://127.0.0.1:8000/api/tramitevisa/",
+                type: "POST",
+                data: JSON.stringify(data),
+                contentType: "application/json",
+                success: function (response) {
+                    localStorage.setItem("Completado", response.access);
+                    alert("Tramite successful!");
+                },
+                error: function (response) {
+                    alert("Tramite failed!");
+                }
+            });
         }
     });
-
-    function subscribe() {
-        $.ajax({
-            url: "http://127.0.0.1:8000/api/tramitevisa/",
-            type: "POST",
-            contentType: "application/json",
-            headers: {
-                Authorization: "Bearer ",
-            },
-            data: JSON.stringify({}),  // No se necesitan datos adicionales
-            success: function (response) {
-                alert("Tramite Aceptado Correctamente.");
-                window.location.href = "MisTramites.html";
-            },
-            error: function (response) {
-                if (response.status === 401) {
-                    alert('Subscription failed! ' + response.responseText);
-                }
-            }
-        });
-    }
 });
+
 
 
 
