324b866a9812679b13962c7f4b60ee3f12571441
Martin <ma.carriel@duocuc.cl>
2024-06-28 09:35:42 -0400

    Ultima evaluacion

--- frontend/js/atentionumber.js
+++ frontend/js/atentionumber.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     // Generar un número aleatorio entre 21 y 100
-    var numeroAleatorio = Math.floor(Math.random() * (100 - 21 + 1)) + 21;
+    const numeroAleatorio = Math.floor(Math.random() * (40 - 30 + 1)) + 30;
 
     // Mostrar el número generado en el elemento con id 'numeroDeAtencion'
     $('#numeroDeAtencion').text(numeroAleatorio);
