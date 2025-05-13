39bcba64565a6edd9fa2af754f74ee6a47a7e8f1
Martin <ma.carriel@duocuc.cl>
2025-05-13 15:50:42 -0400

    Eliminando base de datos anterior.

--- backend/TramiFast/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Tramite, Admin, NumeroAtencion, TramiteCedula
-
-# Register your models here.
-admin.site.register(Tramite)
-admin.site.register(Admin)
-admin.site.register(NumeroAtencion)
-admin.site.register(TramiteCedula)
--- backend/TramiFast/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TramitesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'TramiFast'
