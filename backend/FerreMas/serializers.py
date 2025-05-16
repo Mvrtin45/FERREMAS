23ad3a1f8154ad7d78bae0f57e22ac15d098c7d4
Martin <ma.carriel@duocuc.cl>
2025-05-16 17:21:28 -0400

    serializers.py

--- backend/FerreMas/serializers.py
+++ backend/FerreMas/serializers.py
@@ -1,7 +1,22 @@
-from .models import Herramienta
+from .models import Herramienta, Categoria, Pedido
 from django.contrib.auth.models import User
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
+        fields = '__all__'
+
+class CategoriaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Categoria
+        fields = '__all__'
+
+class HerramientaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Herramienta
+        fields = '__all__'
+
+class PedidoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Pedido
         fields = '__all__'
